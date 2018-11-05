8422a86672cf88b827699d0b8e2bad69096c6b22
baczko <baczkopolter@gmail.com>
2018-11-06 00:10:38 +0100

    Generics part 1

--- src/lectures/part2/oop/Generics.scala
+++ src/lectures/part2/oop/Generics.scala
@@ -13,4 +13,30 @@ object Generics extends App {
   object MyList {
     def empty[A]: MyList[A] = new MyList[A]
   }
+
+  val emptyListOfIntegers = MyList.empty[Int]
+
+  //variance problem
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+
+  // 1. YES. List[Cat] extends List[Animal] = COVARIANCE
+  class CovariantList[+A]
+  val animal: Animal = new Cat
+  val animalList: CovariantList[Animal] = new CovariantList[Cat]
+  //animalList.add(new Dog)??? HARD QUESTION
+
+  //2. NO = INVARIANCE
+  class InvariantList[A]
+  val invariantAnimalList: InvariantList[Animal] =  new InvariantList[Animal]
+
+  //3. Hell, no! CONTRAVARIANCE
+  class ContravariantList[-A]
+  val contravariantList: ContravariantList[Cat] = new ContravariantList[Animal]
+
+  class Trainer[-A]
+  val trainer: Trainer[Cat] = new Trainer[Animal]
+
+
 }
